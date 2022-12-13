06f0bfe070c8d424b0f7ae1f27df7f98a1b24997
Danjing Yin <danjingyin@danjings-air.mynetworksettings.com>
2022-12-13 00:50:14 -0500

    First Init

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'yelp',
+        folder: 'AskingDoc',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });
